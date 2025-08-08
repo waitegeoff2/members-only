eb59aad6c27a58a8a541d6d5f7a3d473b6725bae
Geoffrey Waite <186988887+waitegeoff2@users.noreply.github.com>
2025-08-08 11:58:31 -0400

    Added validator to make sure passwords match.

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -3,15 +3,29 @@ const { body, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
 
 const alphaErr = "must only contain letters.";
+const emptyErr = "is required"
 const lengthErr = "must be between 1 and 50 characters.";
 const emailErr = "must be formatted like an email.";
+const pwMatchErr = "Passwords entered must match"
 
 const validateUser = [
     body("fullname").trim()
+    .notEmpty().withMessage(`Full name ${emptyErr}`)
     .isLength({ min: 1, max: 50 }).withMessage(`Full name ${lengthErr}`),
     body("username").trim()
     .isLength({ min: 1, max: 50 }).withMessage(`Full name ${lengthErr}`)
     .isEmail().withMessage(`Email ${emailErr}`), 
+    body('password')
+    .notEmpty().withMessage(`Password ${emptyErr}`)
+    .isLength({ min: 8 }).withMessage('Password must be at least 8 characters long'),
+    body('confirmpassword')
+        .notEmpty().withMessage('Confirm Password is required')
+        .custom((value, { req }) => {
+            if (value !== req.body.password) {
+                throw new Error('Passwords do not match');
+            }
+            return true;
+        }),
 ]
 
 const addUser = [
